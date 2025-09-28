257e519911b8d1208a9dde68fd13fa84af625fee
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-09-28 16:54:52 +0700

    Update saveProducts.js

--- api/saveProducts.js
+++ api/saveProducts.js
@@ -1,12 +1,24 @@
-import fs from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const filePath = path.join(process.cwd(), 'products.json');
-    fs.writeFileSync(filePath, JSON.stringify(req.body, null, 2));
-    res.status(200).json({ message: 'Produk berhasil disimpan!' });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+export default async function handler(req, res) {
+  const filePath = path.join(process.cwd(), "products.json");
+
+  if (req.method === "GET") {
+    try {
+      const data = await fs.readFile(filePath, "utf8");
+      res.status(200).json(JSON.parse(data));
+    } catch (err) {
+      res.status(500).json({ error: "Gagal membaca products.json" });
+    }
+  }
+
+  if (req.method === "POST") {
+    try {
+      await fs.writeFile(filePath, JSON.stringify(req.body, null, 2));
+      res.status(200).json({ message: "Produk berhasil disimpan" });
+    } catch (err) {
+      res.status(500).json({ error: "Gagal menyimpan produk" });
+    }
   }
 }
