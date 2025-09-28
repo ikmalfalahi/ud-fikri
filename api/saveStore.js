5ccd8a28f0bc0c12ff2e143aa2e24777016717e9
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-09-28 15:42:44 +0700

    Add files via upload

---
+++ api/saveStore.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const filePath = path.join(process.cwd(), 'store.json');
+    fs.writeFileSync(filePath, JSON.stringify(req.body, null, 2));
+    res.status(200).json({ message: 'Info toko berhasil disimpan!' });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
