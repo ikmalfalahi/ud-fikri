ff2704333a71d9ee7e732315de1d3bf153093c60
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-10-24 17:10:49 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,5 +7,8 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  alert(`Login dengan: ${email}`);
+
+  // Nanti dihubungkan ke Supabase di langkah berikut
+  console.log("Email:", email, "Password:", password);
+  alert("Login berhasil (mockup) — nanti dihubungkan ke Supabase ya!");
 });
