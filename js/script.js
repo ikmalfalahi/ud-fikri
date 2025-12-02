a711b625e8521d01b9243d0a8f257be2c8212a7d
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-12-02 14:53:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -569,4 +569,21 @@ if (document.getElementById("user-map")) {
 
 });
 
+// === Back To Top ===
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
 
