e6fac430818bf49efd76b8ce6572bd481c880d2e
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-12-04 23:53:54 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -478,7 +478,7 @@ document.getElementById("checkout").addEventListener("click", () => {
 
   msg += `=====================\n`;
   msg += `_Terima kasih sudah berbelanja 🙏_`;
-  msg += `_https://ud-fikri.vercel.app_`;
+  msg += `*https://ud-fikri.vercel.app*`;
 
   // Kirim ke WA
   window.open(`https://wa.me/6281287505090?text=${encodeURIComponent(msg)}`, "_blank");
@@ -686,5 +686,6 @@ if (document.getElementById("user-map")) {
 }
 
 });
+
 
 
