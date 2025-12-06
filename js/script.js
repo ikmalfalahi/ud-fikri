330081374c97228d8e7c4ff7b42b46c6204c6a2a
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-12-06 14:24:11 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1196,6 +1196,27 @@ if (document.getElementById("user-map")) {
 
 });
 
+// == saklar == //
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    toggle.checked = true;
+  }
+});
 
 
 
