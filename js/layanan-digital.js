35dc9f802e7de53409d1ae16f00b82c585fa03ac
ikmalfalahi <ikmalfalahi99@gmail.com>
2026-01-10 11:04:26 +0700

    Update layanan-digital.js

--- js/layanan-digital.js
+++ js/layanan-digital.js
@@ -225,3 +225,20 @@ function sendWA() {
     "_blank"
   );
 }
+
+/* ================= ACCORDION ================= */
+document.querySelectorAll(".accordion-header").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const item = btn.parentElement;
+    const content = btn.nextElementSibling;
+
+    item.classList.toggle("active");
+
+    if (item.classList.contains("active")) {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      content.style.maxHeight = null;
+    }
+  });
+});
+
