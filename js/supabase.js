f9e67ed5980f2d6f647d82b072edd9e0dfed8e8e
ikmalfalahi <ikmalfalahi99@gmail.com>
2026-01-11 15:21:36 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,11 +2,10 @@
 const SUPABASE_URL = "https://nnohtnywmhuzueamsats.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ub2h0bnl3bWh1enVlYW1zYXRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNjM4NDksImV4cCI6MjA3NDYzOTg0OX0.S8FeDIdXQ32WH9QPVlSsYGRjxYbLMg6HXQicZ35A1pg";
 
-// supaya bisa dipakai di semua file
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
-// opsional: bisa cek apakah berhasil
-console.log("Supabase client siap:", supabase);
+// pastikan supabase sudah ada
+if (!window.supabase) {
+  console.error("Supabase library belum dimuat! Cek CDN script.");
+} else {
+  window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+  console.log("Supabase client siap:", window.supabaseClient);
+}
