88419d2c1509679138aca9ae1ef505d127eaf576
ikmalfalahi <ikmalfalahi99@gmail.com>
2026-01-15 14:39:31 +0700

    Update layanan-digital.js

--- js/layanan-digital.js
+++ js/layanan-digital.js
@@ -412,19 +412,8 @@ data: {
   title: "Transfer Antar Bank",
   placeholder: "Nomor Rekening Tujuan",
   isNominalText: true,
-  adminFee: {
-    rules: [
-      { min: 0, max: 99999, fee: 2500 },
-      { min: 100000, max: 499999, fee: 5000 },
-      { min: 500000, max: 999999, fee: 7500 },
-      { min: 1000000, max: 1000000000, fee: 10000 }
-    ],
-    default: 10000
-  },
-  providers: {
-    BCA: [], BRI: [], BNI: [], Mandiri: [],
-    DANA: [], "GOPAY DRIVER": [], "GOPAY CUSTOMER": [], "OVO": []
-  }
+  adminFee: 10000,
+  providers: { BCA: [], BRI: [], BNI: [], Mandiri: [], DANA: [], "GOPAY DRIVER": [], "GOPAY CUSTOMER": [], "OVO": [] }
 },
 
   title: "Transfer Antar Bank",
